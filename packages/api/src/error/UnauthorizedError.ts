543d7ec3c19ae0ffb0eb34412830de06eb0a400e
Matheus <matheusmendesreis@outlook.com>
2024-02-03 19:19:04 -0300

    build:apagando arquivos desnecessarios

--- packages/api/src/error/UnauthorizedError.ts
+++
@@ -1,9 +0,0 @@
-import { BaseError } from "./BaseError";
-
-export class UnauthorizedError extends BaseError {
-    constructor(
-      message: string = "Acesso não autorizado" // mensagem de erro padrão caso não seja enviado um argumento
-    ) {
-      super(401, message);
-    }
-  }
--- packages/api/src/error/ValidationError.ts
+++
@@ -1,9 +0,0 @@
-import { BaseError } from "./BaseError";
-
-export class ValidationError extends BaseError {
-    constructor(
-      message: string = "Erro de validação" // mensagem de erro padrão caso não seja enviado um argumento
-    ) {
-      super(422, message);
-    }
-  }
